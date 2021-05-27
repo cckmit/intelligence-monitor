dec2b9bc24d167a9a1d830a3cefba9d8710be643
liukai <liukai@zhikuntech.com>
2021-05-27 16:00:04 +0800

    add 注册中心

---
+++ unityplatform-microregistrycenter/src/main/java/com/zhikuntech/intellimonitor/RegistryCenterApplication.java
@@ -0,0 +1,19 @@
+package com.zhikuntech.intellimonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心
+ *
+ * @author liukai
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryCenterApplication.class, args);
+    }
+}
