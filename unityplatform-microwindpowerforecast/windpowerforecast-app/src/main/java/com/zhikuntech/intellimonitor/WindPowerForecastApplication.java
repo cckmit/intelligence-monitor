14157eb10463b517cda5ff7985f0ca32bf2c481a
liukai <liukai@zhikuntech.com>
2021-05-27 14:13:45 +0800

    init commit

---
+++ unityplatform-microwindpowerforecast/windpowerforecast-app/src/main/java/com/zhikuntech/intellimonitor/WindPowerForecastApplication.java
@@ -0,0 +1,16 @@
+package com.zhikuntech.intellimonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liukai
+ */
+@SpringBootApplication
+public class WindPowerForecastApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WindPowerForecastApplication.class);
+    }
+}
