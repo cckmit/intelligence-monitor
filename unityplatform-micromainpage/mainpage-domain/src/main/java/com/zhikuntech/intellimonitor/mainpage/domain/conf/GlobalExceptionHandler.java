d550e290918ca0e5fbbaa0afef2a3751367eaa64
jchonker <jchonker@163.com>
2021-06-08 19:07:27 +0800

    增加全局异常处理类

---
+++ unityplatform-micromainpage/mainpage-domain/src/main/java/com/zhikuntech/intellimonitor/mainpage/domain/conf/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.zhikuntech.intellimonitor.mainpage.domain.conf;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @Author 杨锦程
+ * @Date 2021/6/8 18:29
+ * @Description 全局异常处理
+ * @Version 1.0
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
