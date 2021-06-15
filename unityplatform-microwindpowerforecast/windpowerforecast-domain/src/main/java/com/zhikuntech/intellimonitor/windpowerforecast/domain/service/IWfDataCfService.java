43656f48b826cb3e474a1361418e1e4fbebdfa2d
liukai <liukai@zhikuntech.com>
2021-06-15 18:35:13 +0800

    add 实测风数据

---
+++ unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/service/IWfDataCfService.java
@@ -0,0 +1,16 @@
+package com.zhikuntech.intellimonitor.windpowerforecast.domain.service;
+
+import com.zhikuntech.intellimonitor.windpowerforecast.domain.entity.WfDataCf;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 实测气象 服务类
+ * </p>
+ *
+ * @author liukai
+ * @since 2021-06-15
+ */
+public interface IWfDataCfService extends IService<WfDataCf> {
+
+}
