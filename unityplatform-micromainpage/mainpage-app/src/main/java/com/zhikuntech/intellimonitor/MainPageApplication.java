d06ef818fabbef0c6f75fd8a379a432d7666d1db
yangjincheng <jchonker@163.com>
2021-06-15 19:17:45 +0800

    add mybatis的注解MapperScan

--- unityplatform-micromainpage/mainpage-app/src/main/java/com/zhikuntech/intellimonitor/MainPageApplication.java
+++ unityplatform-micromainpage/mainpage-app/src/main/java/com/zhikuntech/intellimonitor/MainPageApplication.java
@@ -1,5 +1,6 @@
 package com.zhikuntech.intellimonitor;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.zhikuntech.intellimonitor.mainpage.domain.mapper")
 public class MainPageApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainPageApplication.class, args);
