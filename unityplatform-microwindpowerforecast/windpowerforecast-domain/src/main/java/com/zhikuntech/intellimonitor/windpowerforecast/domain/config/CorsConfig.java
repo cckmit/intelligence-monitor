9006d7989db24a8403c9ca2880a8f4f1245a35e3
liukai <liukai@zhikuntech.com>
2021-06-16 18:29:57 +0800

    add 跨域配置

---
+++ unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.zhikuntech.intellimonitor.windpowerforecast.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author liukai
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("POST", "GET", "OPTION")
+                .allowedOrigins("*");
+    }
+}
