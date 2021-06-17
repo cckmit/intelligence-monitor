3b896cf2f3f7f4ef5f221a775de5e5a2ad85492d
Dai808 <dai808@github.com>
2021-06-17 16:30:30 +0800

    添加 获取风机详情分图二数据

---
+++ unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/mapper/FsBasicParameterMapper.java
@@ -0,0 +1,13 @@
+package com.zhikuntech.intellimonitor.fanscada.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhikuntech.intellimonitor.fanscada.domain.vo.FsBasicParameterVO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author： DAI
+ * @date： Created in 2021/6/17 10:46
+ */
+@Mapper
+public interface FsBasicParameterMapper extends BaseMapper<FsBasicParameterVO> {
+}
