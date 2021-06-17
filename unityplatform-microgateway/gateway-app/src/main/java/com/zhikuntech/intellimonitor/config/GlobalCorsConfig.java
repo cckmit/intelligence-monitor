d758a630f0fa445e02cca93a2029910abb9eac87
yangjincheng <jchonker@163.com>
2021-06-17 11:40:15 +0800

    add 网关配置

---
+++ unityplatform-microgateway/gateway-app/src/main/java/com/zhikuntech/intellimonitor/config/GlobalCorsConfig.java
@@ -0,0 +1,30 @@
+package com.zhikuntech.intellimonitor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @Author 杨锦程
+ * @Date 2021/6/16 18:51
+ * @Description 全局跨域配置
+ * @Version 1.0
+ */
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
