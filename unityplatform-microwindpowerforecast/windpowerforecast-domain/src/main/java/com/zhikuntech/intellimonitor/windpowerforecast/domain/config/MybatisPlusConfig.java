444ea4fcd08be0391a609397d222b4780ac66118
liukai <liukai@zhikuntech.com>
2021-06-19 14:42:49 +0800

    fix MybatisPlusInterceptor分页插件有问题

--- unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/config/MybatisPlusConfig.java
+++ unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/config/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.zhikuntech.intellimonitor.windpowerforecast.domain.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +14,29 @@ import org.springframework.context.annotation.Configuration;
 public class MybatisPlusConfig {
 
 
+
+
+//    /**
+//     * mybatis-plus 分页插件配置
+//     * @return {@link MybatisPlusInterceptor}
+//     */
+//    @Bean
+//    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+//        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+//        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+//        return interceptor;
+//    }
+
+
     /**
-     * mybatis-plus 分页插件配置
-     * @return {@link MybatisPlusInterceptor}
+     * 需要需要这个拦截器, {@link MybatisPlusInterceptor}这个拦截器分页有bug
+     * @return 拦截器
      */
     @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-        return interceptor;
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setDbType(DbType.MYSQL);
+        return paginationInterceptor;
     }
 
 }
