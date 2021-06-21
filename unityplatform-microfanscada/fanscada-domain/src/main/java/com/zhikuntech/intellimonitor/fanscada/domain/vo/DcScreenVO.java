5050687029e6cca3249b1203143bb6e8109131a7
Dai808 <dai808@github.com>
2021-06-21 16:10:04 +0800

    修改 返回小数转字符串

--- unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/vo/DcScreenVO.java
+++ unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/vo/DcScreenVO.java
@@ -1,5 +1,6 @@
 package com.zhikuntech.intellimonitor.fanscada.domain.vo;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.zhikuntech.intellimonitor.fanscada.domain.golden.annotation.GoldenId;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -20,69 +21,86 @@ public class DcScreenVO {
 
     @GoldenId(value = 76)
     @ApiModelProperty("直流屏电池组电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal batteryPackVoltage;
 
     @GoldenId(value = 77)
     @ApiModelProperty("直流屏电池组温度")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal batteryPackTemperature;
 
     @GoldenId(value = 15)
     @ApiModelProperty("直流屏负载电流")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal loadCurrent;
 
     @GoldenId(value = 16)
     @ApiModelProperty("直流屏母线负对地电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal negativeVoltage;
 
     @GoldenId(value = 17)
     @ApiModelProperty("直流屏母线负对地电阻")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal negativeResistance;
 
     @GoldenId(value = 18)
     @ApiModelProperty("直流屏主机交流输入电压A相")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal voltagePhaseA;
 
     @GoldenId(value = 19)
     @ApiModelProperty("直流屏主机交流输入电压C相")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal voltagePhaseC;
 
     @GoldenId(value = 20)
     @ApiModelProperty("直流屏02#UPS/NV输出电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal outputVoltage2;
 
     @GoldenId(value = 21)
     @ApiModelProperty("直流屏02#UPS/NV输出电流")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal outputCurrent2;
 
     @GoldenId(value = 22)
     @ApiModelProperty("直流屏电池组电流")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal batteryPackCurrent;
 
     @GoldenId(value = 23)
     @ApiModelProperty("直流屏母线电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal busVoltage;
 
     @GoldenId(value = 24)
     @ApiModelProperty("直流屏母线正对地电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal ratedOutputVoltage;
 
     @GoldenId(value = 25)
     @ApiModelProperty("直流屏母线正对地电阻")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal positiveVoltage;
 
     @GoldenId(value = 26)
     @ApiModelProperty("直流屏母线对地交流电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal acVoltage;
 
     @GoldenId(value = 27)
     @ApiModelProperty("直流屏主机交流输入电压B相")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal voltagePhaseB;
 
     @GoldenId(value = 28)
     @ApiModelProperty("直流屏01#UPS/NV输出电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal outputVoltage1;
 
     @GoldenId(value = 29)
     @ApiModelProperty("直流屏01#UPS/NV输出电流")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal outputCurrent1;
 }
--- unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/vo/UpsTelemetryVO.java
+++ unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/vo/UpsTelemetryVO.java
@@ -1,5 +1,6 @@
 package com.zhikuntech.intellimonitor.fanscada.domain.vo;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.zhikuntech.intellimonitor.fanscada.domain.golden.annotation.GoldenId;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -20,53 +21,66 @@ public class UpsTelemetryVO {
 
     @GoldenId(value = 30)
     @ApiModelProperty("UPS输入电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal inputVoltage;
 
     @GoldenId(value = 64)
     @ApiModelProperty("UPS输出电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal outputVoltage;
 
     @GoldenId(value = 65)
     @ApiModelProperty("UPS旁路功率")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal bypassPower;
 
     @GoldenId(value = 66)
     @ApiModelProperty("UPS输出电流")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal outputCurrent;
 
     @GoldenId(value = 67)
     @ApiModelProperty("UPS环境温度")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal ambientTemperature;
 
     @GoldenId(value = 68)
     @ApiModelProperty("UPS额定输出电流")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal ratedOutputCurrent;
 
     @GoldenId(value = 69)
     @ApiModelProperty("UPS额定频率")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal ratedFrequency;
 
     @GoldenId(value = 70)
     @ApiModelProperty("UPS旁路电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal bypassVoltage;
 
     @GoldenId(value = 71)
     @ApiModelProperty("UPS输入功率")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal inputPower;
 
     @GoldenId(value = 72)
     @ApiModelProperty("UPS输出功率")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal outputPower;
 
     @GoldenId(value = 73)
     @ApiModelProperty("UPS电池电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal batteryVoltage;
 
     @GoldenId(value = 74)
     @ApiModelProperty("UPS额定输出电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal ratedOutputVoltage;
 
     @GoldenId(value = 75)
     @ApiModelProperty("UPS额定电池电压")
+    @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
     private BigDecimal ratedBatteryVoltage;
 }
