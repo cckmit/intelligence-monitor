1909e013223ff540204f51a2adcae64f1b6feeea
liukai <liukai@zhikuntech.com>
2021-06-21 13:56:38 +0800

    add 考核结果-月数据

---
+++ unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/service/IWfAssessMonthService.java
@@ -0,0 +1,16 @@
+package com.zhikuntech.intellimonitor.windpowerforecast.domain.service;
+
+import com.zhikuntech.intellimonitor.windpowerforecast.domain.entity.WfAssessMonth;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 月考核结果 服务类
+ * </p>
+ *
+ * @author liukai
+ * @since 2021-06-21
+ */
+public interface IWfAssessMonthService extends IService<WfAssessMonth> {
+
+}
---
+++ unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/service/impl/WfAssessMonthServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zhikuntech.intellimonitor.windpowerforecast.domain.service.impl;
+
+import com.zhikuntech.intellimonitor.windpowerforecast.domain.entity.WfAssessMonth;
+import com.zhikuntech.intellimonitor.windpowerforecast.domain.mapper.WfAssessMonthMapper;
+import com.zhikuntech.intellimonitor.windpowerforecast.domain.service.IWfAssessMonthService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 月考核结果 服务实现类
+ * </p>
+ *
+ * @author liukai
+ * @since 2021-06-21
+ */
+@Service
+public class WfAssessMonthServiceImpl extends ServiceImpl<WfAssessMonthMapper, WfAssessMonth> implements IWfAssessMonthService {
+
+}
