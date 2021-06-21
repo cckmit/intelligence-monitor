a0b53fe6099084185f798907b5c21eafaececd36
Dai808 <dai808@github.com>
2021-06-21 14:46:15 +0800

    修改 从数据库获取goldenId

--- unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/golden/InjectPropertiesUtil.java
+++ unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/golden/InjectPropertiesUtil.java
@@ -1,10 +1,13 @@
 package com.zhikuntech.intellimonitor.fanscada.domain.golden;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.rtdb.api.model.RtdbData;
 import com.rtdb.api.model.ValueData;
 import com.zhikuntech.intellimonitor.core.commons.constant.FanConstant;
 import com.zhikuntech.intellimonitor.fanscada.domain.golden.annotation.GoldenId;
+import com.zhikuntech.intellimonitor.fanscada.domain.mapper.BackendToGoldenMapper;
+import com.zhikuntech.intellimonitor.fanscada.domain.pojo.BackendToGolden;
 import com.zhikuntech.intellimonitor.fanscada.domain.service.BackendToGoldenService;
 import com.zhikuntech.intellimonitor.fanscada.domain.service.impl.BackendToGoldenServiceImpl;
 import com.zhikuntech.intellimonitor.fanscada.domain.utils.RedisUtil;
@@ -13,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.CollectionUtils;
 
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
@@ -192,38 +196,74 @@ public class InjectPropertiesUtil<T> {
         return t;
     }
 
-    public static <T> T injectByAnnotationCustomize(T t, List<ValueData> data) {
+    /**
+     * @param t      映射实体
+     * @param number 风机编号
+     * @param mapper BackendToGoldenMapper
+     * @return : T
+     * @date: Creat in 2021/6/21 11:43
+     * @describe: 获取庚顿数据
+     */
+    public static <T> T injectByAnnotationCustomize(T t, String number, BackendToGoldenMapper mapper) {
+        long start = System.currentTimeMillis();
+        GoldenUtil goldenUtil = new GoldenUtil();
+        // 获取注解的backendList
         Field[] fields = t.getClass().getDeclaredFields();
+        List<Integer> backendList = new ArrayList<>();
         for (Field field : fields) {
             if (field.getAnnotation(GoldenId.class) != null) {
-                GoldenId goldenId = field.getDeclaredAnnotation(GoldenId.class);
-                int value = null == goldenId ? 0 : goldenId.value();
-                for (ValueData valueData : data) {
-                    if (value == valueData.getId()) {
-                        try {
-                            field.setAccessible(true);
-                            Double dataValue = valueData.getValue();
-                            Class<?> fieldType = field.getType();
-                            if (BigDecimal.class.equals(fieldType)) {
-                                BigDecimal bigDecimal = BigDecimal.valueOf(dataValue).setScale(2, RoundingMode.HALF_UP);
-                                field.set(t, bigDecimal);
-                            } else if (Integer.class.equals(fieldType)) {
-                                field.set(t, dataValue.intValue());
-                            } else if (String.class.equals(fieldType)) {
-                                field.set(t, String.valueOf(dataValue));
-                            } else {
-                                field.set(t, null);
-                            }
+                int value = field.getAnnotation(GoldenId.class).value();
+                backendList.add(value);
+            }
+        }
+        if (CollectionUtils.isEmpty(backendList)) {
+            return null;
+        }
+        // 通过风机编号查询GoldenIdList
+        List<Integer> goldenIdList = mapper.getGoldenIdByWindNumberAndId(backendList);
+        int[] goldenIds = goldenIdList.stream().mapToInt(Integer::intValue).toArray();
 
-                        } catch (IllegalAccessException e) {
-                            e.printStackTrace();
-                            return null;
+        QueryWrapper<BackendToGolden> query = new QueryWrapper<>();
+        query.eq("number", number);
+        List<BackendToGolden> backendToGoldenList = mapper.selectList(query);
+        try {
+            List<ValueData> valueDataList = goldenUtil.getSnapshots(goldenIds);
+            for (Field field : fields) {
+                int fieldValue = field.getAnnotation(GoldenId.class).value();
+                if (field.getAnnotation(GoldenId.class) == null) {
+                    continue;
+                }
+                BackendToGolden backend = null;
+                for (BackendToGolden backendToGolden : backendToGoldenList) {
+                    if (backendToGolden.getBackendId() == fieldValue) {
+                        backend = backendToGolden;
+                    }
+                }
+                if (backend == null) {
+                    continue;
+                }
+                for (ValueData valueData : valueDataList) {
+                    if (valueData.getId() == backend.getGoldenId()) {
+                        field.setAccessible(true);
+                        Double dataValue = valueData.getValue();
+                        Class<?> fieldType = field.getType();
+                        if (BigDecimal.class.equals(fieldType)) {
+                            BigDecimal bigDecimal = BigDecimal.valueOf(dataValue).setScale(2, RoundingMode.HALF_UP);
+                            field.set(t, bigDecimal);
+                        } else if (Integer.class.equals(fieldType)) {
+                            field.set(t, dataValue.intValue());
+                        } else if (String.class.equals(fieldType)) {
+                            field.set(t, String.valueOf(dataValue));
+                        } else {
+                            field.set(t, null);
                         }
                     }
                 }
             }
+            log.info("##############injectByAnnotationCustomize方法执行完毕，用时：{}{}", System.currentTimeMillis() - start, "毫秒");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return t;
     }
-
 }
