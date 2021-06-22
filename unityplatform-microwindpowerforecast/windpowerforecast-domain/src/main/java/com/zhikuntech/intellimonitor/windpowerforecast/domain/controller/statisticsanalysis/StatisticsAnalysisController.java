3cd5096acdf703e404c1b6aabff685c4be140d43
caojinlong <koala19991108cjl@163.com>
2021-06-22 18:36:23 +0800

    redisson锁

--- unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/controller/statisticsanalysis/StatisticsAnalysisController.java
+++ unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/controller/statisticsanalysis/StatisticsAnalysisController.java
@@ -44,23 +44,8 @@ public class StatisticsAnalysisController {
     @ApiOperation("功率分析-短期功率")
     @PostMapping("/query-dq")
     public BaseResponse<DqListAggregateDTO> dqPowerAnalysis(@RequestBody PowerAnalysisQuery query) {
-
-        String redisKey = "redis_key-1";
-        RLock redissonLock = redissonClient.getLock(redisKey);
-        try {
-            redissonLock.lock();
-            System.out.println("获取到redis锁");
-            //获取到redis锁，进行业务逻辑
-            DqListAggregateDTO result = dqService.dqPowerAnalysis(query);
-            return BaseResponse.success(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            redissonLock.unlock();
-            System.out.println("解开redis锁");
-        }
-
+        DqListAggregateDTO result = dqService.dqPowerAnalysis(query);
+        return BaseResponse.success(result);
     }
 
 
@@ -69,59 +54,22 @@ public class StatisticsAnalysisController {
     @ApiOperation("功率分析-超短期功率")
     @PostMapping("/query-cdq")
     public BaseResponse<CdqListAggregateDTO> cdqPowerAnalysis(@RequestBody PowerAnalysisQuery query) {
-        String redisKey = "redis_key-2";
-        RLock redissonLock = redissonClient.getLock(redisKey);
-        try {
-            redissonLock.lock();
-            //获取到redis锁，进行业务逻辑
-            System.out.println("获取到redis锁");
-            CdqListAggregateDTO result = cdqService.cdqPowerAnalysis(query);
-            return BaseResponse.success(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            redissonLock.unlock();
-            System.out.println("解开redis锁");
-        }
+        CdqListAggregateDTO result = cdqService.cdqPowerAnalysis(query);
+        return BaseResponse.success(result);
     }
+
 
     @ApiOperation("功率分析-短期功率-曲线")
     @PostMapping("/query-dq-curve")
     public BaseResponse<List<DqPowerAnalysisDTO>> dqPowerAnalysisCurve(@RequestBody PowerAnalysisQuery query) {
-        String redisKey = "redis_key-3";
-        RLock redissonLock = redissonClient.getLock(redisKey);
-        try {
-            redissonLock.lock();
-            System.out.println("获取到redis锁");
-            //获取到redis锁，进行业务逻辑
-            List<DqPowerAnalysisDTO> result = dqService.DqPowerAnalysisCurve(query);
-            return BaseResponse.success(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            redissonLock.unlock();
-            System.out.println("解开redis锁");
-        }
+       List<DqPowerAnalysisDTO> result = dqService.DqPowerAnalysisCurve(query);
+       return BaseResponse.success(result);
     }
+
     @ApiOperation("功率分析-超短期功率-曲线")
     @PostMapping("/query-cdq-curve")
     public BaseResponse<List<CdqPowerAnalysisDTO>> cdqPowerAnalysisCurve(@RequestBody PowerAnalysisQuery query) {
-        String redisKey = "redis_key-4";
-        RLock redissonLock = redissonClient.getLock(redisKey);
-        try {
-            redissonLock.lock();
-            System.out.println("获取到redis锁");
-            //获取到redis锁，进行业务逻辑
-            List<CdqPowerAnalysisDTO> result = cdqService.cdqPowerAnalysisCurve(query);
-            return BaseResponse.success(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            redissonLock.unlock();
-            System.out.println("解开redis锁");
-        }
+        List<CdqPowerAnalysisDTO> result = cdqService.cdqPowerAnalysisCurve(query);
+        return BaseResponse.success(result);
     }
 }
--- unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/service/impl/WfAssessMonthServiceImpl.java
+++ unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/service/impl/WfAssessMonthServiceImpl.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zhikuntech.intellimonitor.core.commons.base.Pager;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.dto.assessresult.MonthAssessCurveDTO;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.dto.assessresult.MonthAssessListDTO;
+import com.zhikuntech.intellimonitor.windpowerforecast.domain.dto.statisticsanalysis.DqPowerAnalysisDTO;
+import com.zhikuntech.intellimonitor.windpowerforecast.domain.entity.WfAnalyseDq;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.entity.WfAssessMonth;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.mapper.WfAssessMonthMapper;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.query.assessresult.MonthAssessQuery;
@@ -13,7 +15,9 @@ import com.zhikuntech.intellimonitor.windpowerforecast.domain.service.IWfAssessM
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.utils.DateProcessUtils;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.utils.TimeProcessUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -118,9 +122,58 @@ public class WfAssessMonthServiceImpl extends ServiceImpl<WfAssessMonthMapper, W
     @Override
     public List<MonthAssessCurveDTO> queryMonthCurve(MonthCurveQuery query) {
         // TODO
-
-        return new ArrayList<>();
+        //list<显示数据>
+        List<MonthAssessCurveDTO> result = new ArrayList<>();
+        if (Objects.isNull(query)) {
+            return result;
+        }
+        String queryMod = query.getQueryMod();//查询模式
+        String queryYearPre = query.getQueryYearPre();//前年
+        String queryYearPost = query.getQueryYearPost();//后年
+        //构建query
+        QueryWrapper<WfAssessMonth> criteria = new QueryWrapper<>();
+        if (StringUtils.equalsIgnoreCase(queryMod, MonthAssessQuery.QUERY_ONE)) {//如果模式是one
+            // check
+            if (StringUtils.isBlank(queryYearPre)) {
+                return result;
+            }
+            String yearStr = queryYearPre + "-01-01";//前年
+            LocalDate curYear = DateProcessUtils.parseToLocalDate(yearStr);//开始时间
+            LocalDate nextYear = curYear.plusYears(1);//结束时间
+            criteria.ge("calc_date", TimeProcessUtils.formatLocalDateTimeWithSecondPattern(curYear));//大于等于
+            criteria.lt("calc_date", TimeProcessUtils.formatLocalDateTimeWithSecondPattern(nextYear));//小于
+        } else {
+            // check
+            if (StringUtils.isBlank(queryYearPre) || StringUtils.isBlank(queryYearPost)) {
+                return result;
+            }
+            String yearStrPre = queryYearPre + "-01-01";//开始时间
+            String yearStrPost = queryYearPost + "-01-01";//结束时间
+            LocalDate preYear = DateProcessUtils.parseToLocalDate(yearStrPre);//开始时间
+            LocalDate postYear = DateProcessUtils.parseToLocalDate(yearStrPost);//结束时间
+            postYear = postYear.plusDays(1);//结束时间加一
+            criteria.ge("calc_date", TimeProcessUtils.formatLocalDateTimeWithSecondPattern(preYear));//大于等于
+            criteria.lt("calc_date", TimeProcessUtils.formatLocalDateTimeWithSecondPattern(postYear));//小于
+        }
+        // 查询
+        List<WfAssessMonth> fromDb=getBaseMapper().selectList(criteria);
+        //转换结果
+        if (CollectionUtils.isNotEmpty(fromDb)) {//判断fromDb不为空
+            List<MonthAssessCurveDTO> tmp = new ArrayList<>();//创建存MonthAssessCurveDTO的list集合叫tmp
+            for (WfAssessMonth a : fromDb) {//遍历fromDb
+                if (Objects.isNull(a)) {//判断fromDb的这个位置不为null
+                    tmp.add(null);
+                }
+                if (Objects.nonNull(a)) {//如果a不为null
+                    MonthAssessCurveDTO dtoTmp = new MonthAssessCurveDTO();//创建MonthAssessCurveDTO对象dtoTmp
+                    BeanUtils.copyProperties(a, dtoTmp);//a转成dtoTmp类
+                    tmp.add(dtoTmp);//dtoTmp存进tmp
+                }
+            }
+            result=tmp;
+            return result;
+        }
+        return result;
     }
-
 
 }
