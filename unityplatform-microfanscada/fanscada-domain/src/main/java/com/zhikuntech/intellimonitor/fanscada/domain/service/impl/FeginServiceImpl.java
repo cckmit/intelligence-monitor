19abc5c34ecfb7d2f983f4d2d17dc670c7987e61
tenan_TT <8181937+tenan-tt@user.noreply.gitee.com>
2021-06-24 18:07:02 +0800

    fegin修改

--- unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/service/impl/FeginServiceImpl.java
+++ unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/service/impl/FeginServiceImpl.java
@@ -1,11 +1,15 @@
 package com.zhikuntech.intellimonitor.fanscada.domain.service.impl;
 
 
+import com.rtdb.api.model.ValueData;
+import com.zhikuntech.intellimonitor.fanscada.domain.golden.GoldenUtil;
 import com.zhikuntech.intellimonitor.fanscada.domain.service.FeginService;
 import com.zhikuntech.intellimonitor.fanscada.prototype.dto.ActPowerDataDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author 滕楠
@@ -16,14 +20,27 @@ import java.math.BigDecimal;
 public class FeginServiceImpl implements FeginService {
 
 
+    @Autowired
+    private GoldenUtil goldenUtil;
     @Override
     public ActPowerDataDTO getPowerSum() {
 
         //获取风机状态,获取有功功率,做和
+        int[] a = {12};
+        try {
+            List<ValueData> snapshots = goldenUtil.getSnapshots(a);
+            ValueData valueData = snapshots.get(0);
+            Double value = valueData.getValue();
+            ActPowerDataDTO actPowerDataDTO = new ActPowerDataDTO();
+            actPowerDataDTO.setActualProduce(BigDecimal.valueOf(value));
+            return actPowerDataDTO;
+        } catch (Exception e) {
+            e.printStackTrace();
+            ActPowerDataDTO actPowerDataDTO = new ActPowerDataDTO();
+            actPowerDataDTO.setActualProduce(BigDecimal.valueOf(0.0));
+            return actPowerDataDTO;
+        }
 
 
-        ActPowerDataDTO actPowerDataDTO = new ActPowerDataDTO();
-        actPowerDataDTO.setActualProduce(BigDecimal.valueOf(0.0));
-        return actPowerDataDTO;
     }
 }
