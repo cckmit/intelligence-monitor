624706c453bfa13f49836b4abd09729db6748791
yangjincheng <jchonker@163.com>
2021-06-25 10:42:13 +0800

    update 对接风功率曲线的接口

--- unityplatform-micromainpage/mainpage-domain/src/main/java/com/zhikuntech/intellimonitor/mainpage/domain/controller/WinPowerCurveController.java
+++ unityplatform-micromainpage/mainpage-domain/src/main/java/com/zhikuntech/intellimonitor/mainpage/domain/controller/WinPowerCurveController.java
@@ -1,18 +1,14 @@
 package com.zhikuntech.intellimonitor.mainpage.domain.controller;
 
 import com.zhikuntech.intellimonitor.core.commons.base.BaseResponse;
-import com.zhikuntech.intellimonitor.mainpage.domain.service.WinPowerCurveService;
-import com.zhikuntech.intellimonitor.mainpage.domain.vo.TimePowerVO;
-import com.zhikuntech.intellimonitor.mainpage.domain.vo.TimeWindSpeedVO;
-import com.zhikuntech.intellimonitor.mainpage.domain.vo.WindPowerCurveVO;
+import com.zhikuntech.intellimonitor.windpowerforecast.facade.ForeCastCurveFacade;
+import com.zhikuntech.intellimonitor.windpowerforecast.prototype.dto.NwpListPatternDTO;
+import com.zhikuntech.intellimonitor.windpowerforecast.prototype.query.NwpCurvePatternQuery;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,64 +21,16 @@ import java.util.List;
 @Api(tags = "WinPowerCurveController",description = "风功率曲线")
 @RestController
 @RequestMapping("/winPowerCurve")
+@Slf4j
 public class WinPowerCurveController {
     @Autowired
-    private WinPowerCurveService winPowerCurveService;
+    private ForeCastCurveFacade foreCastCurveFacade;
 
-    @GetMapping("/getAll")
+    @PostMapping("/getAll")
     @ApiOperation("获取所有时间内的【风功率曲线】数据")
-    public BaseResponse<WindPowerCurveVO> getWindPowerCurveOfAllTime(){
-        WindPowerCurveVO windPowerCurveVO = winPowerCurveService.getWindPowerCurveOfAllTime();
-        return BaseResponse.success(windPowerCurveVO);
-    }
-
-    @GetMapping("/getCurrent")
-    @ApiOperation("获取庚顿数据库中当前【风功率曲线】数据")
-    public BaseResponse<WindPowerCurveVO> getWindPowerCurve() throws Exception {
-        WindPowerCurveVO windPowerCurveVO = winPowerCurveService.getWindPowerCurve();
-        return BaseResponse.success(windPowerCurveVO);
-    }
-
-    @GetMapping("/subscribe/{username}")
-    @ApiOperation("订阅【风功率曲线】相关标签点快照改变的通知")
-    @ApiImplicitParam(name = "username",value = "登录用户名")
-    public BaseResponse<Boolean> subscribeWindPowerCurve(@PathVariable("username") String username) throws Exception {
-        boolean result = winPowerCurveService.subscribeWindPowerCurve(username);
-        return BaseResponse.success(result);
-    }
-
-    @GetMapping("/getShortTermForecastPower")
-    @ApiOperation("获取短期预测功率")
-    public BaseResponse getShortTermForecastPower(){
-        List<TimePowerVO> shortTermForecastPower = winPowerCurveService.getShortTermForecastPower();
-        return BaseResponse.success(shortTermForecastPower);
-    }
-
-    @GetMapping("/getSupShortTermForecastPower")
-    @ApiOperation("获取超短期预测功率")
-    public BaseResponse getSupShortTermForecastPower(){
-        List<TimePowerVO> supShortTermForecastPower = winPowerCurveService.getSupShortTermForecastPower();
-        return BaseResponse.success(supShortTermForecastPower);
-    }
-
-    @GetMapping("/getActualPower")
-    @ApiOperation("获取实际功率")
-    public BaseResponse getActualPower(){
-        List<TimePowerVO> actualPower = winPowerCurveService.getActualPower();
-        return BaseResponse.success(actualPower);
-    }
-
-    @GetMapping("/getWeatherForecastPower")
-    @ApiOperation("获取天气预报风速")
-    public BaseResponse getWeatherForecastPower(){
-        List<TimeWindSpeedVO> weatherForecastPower = winPowerCurveService.getWeatherForecastPower();
-        return BaseResponse.success(weatherForecastPower);
-    }
-
-    @GetMapping("/getMeasuredWindSpeed")
-    @ApiOperation("获取实测风速")
-    public BaseResponse getMeasuredWindSpeed(){
-        List<TimeWindSpeedVO> measuredWindSpeedList = winPowerCurveService.getMeasuredWindSpeed();
-        return BaseResponse.success(measuredWindSpeedList);
+    public BaseResponse getWindPowerCurveOfAllTime(@RequestBody NwpCurvePatternQuery nwpCurvePatternQuery){
+        BaseResponse<List<NwpListPatternDTO>> listBaseResponse = foreCastCurveFacade.nwpCurveQuery(nwpCurvePatternQuery);
+        log.info("获取所有时间内的【风功率曲线】数据 nwpListPatternDTOS:{}{}","\n",listBaseResponse);
+        return listBaseResponse;
     }
 }
