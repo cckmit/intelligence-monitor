8930c93d028869aa08ddbee44b09efe3359f4fbb
tenan_TT <8181937+tenan-tt@user.noreply.gitee.com>
2021-06-28 10:41:31 +0800

    优化

--- unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/pojo/SocketParam.java
+++ unityplatform-microfanscada/fanscada-domain/src/main/java/com/zhikuntech/intellimonitor/fanscada/domain/pojo/SocketParam.java
@@ -1,6 +1,7 @@
 package com.zhikuntech.intellimonitor.fanscada.domain.pojo;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -12,11 +13,12 @@ import lombok.Data;
 @ApiModel
 public class SocketParam {
 
+    @ApiModelProperty("用户名")
     private String username;
-
+    @ApiModelProperty("session")
     private String sessionId;
-
+    @ApiModelProperty("消息内容")
     private String message;
-
+    @ApiModelProperty("订阅模式,默认01")
     private String messageType;
 }
