77bebec4b6c00bc5509755f0dffc7d2bcd53355f
root <caojinlong17.com>
2021-06-28 19:41:56 +0800

    超短期

--- unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/service/impl/analysis/WfAnalyseCdqServiceImpl.java
+++ unityplatform-microwindpowerforecast/windpowerforecast-domain/src/main/java/com/zhikuntech/intellimonitor/windpowerforecast/domain/service/impl/analysis/WfAnalyseCdqServiceImpl.java
@@ -6,7 +6,9 @@ import com.zhikuntech.intellimonitor.core.commons.base.Pager;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.dto.statisticsanalysis.AvgPowerAnalysisDTO;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.dto.statisticsanalysis.CdqListAggregateDTO;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.dto.statisticsanalysis.CdqPowerAnalysisDTO;
+import com.zhikuntech.intellimonitor.windpowerforecast.domain.dto.statisticsanalysis.DqPowerAnalysisDTO;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.entity.WfAnalyseCdq;
+import com.zhikuntech.intellimonitor.windpowerforecast.domain.entity.WfAnalyseDq;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.mapper.WfAnalyseCdqMapper;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.query.statisticsanalysis.PowerAnalysisQuery;
 import com.zhikuntech.intellimonitor.windpowerforecast.domain.service.analysis.IWfAnalyseCdqService;
@@ -48,19 +50,16 @@ public class WfAnalyseCdqServiceImpl extends ServiceImpl<WfAnalyseCdqMapper, WfA
         if (Objects.isNull(query.getQueryMode())) {
             throw new IllegalArgumentException("查询模式不能为空");
         }
-
         String dateStrPre = query.getDateStrPre();
-        String dateStrPost = query.getDateStrPost();
         LocalDateTime pre = DateProcessUtils.parseToLocalDateTime(dateStrPre);
-        LocalDateTime post = DateProcessUtils.parseToLocalDateTime(dateStrPost);
-        if (pre == null || post == null) {
-            return aggregateDTO;
-        }
-        String preStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(pre);
-        String postStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(post.plusDays(1));
+        String preStr;
+        String postStr ;
         String queryMode=query.getQueryMode();
         if (queryMode.equals("day")){
-            postStr=postStr;
+            String dateStrPost = query.getDateStrPost();
+            LocalDateTime post = DateProcessUtils.parseToLocalDateTime(dateStrPost);
+            preStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(pre);
+            postStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(post.plusDays(1));
         }else if (queryMode.equals("month")){
             LocalDateTime first=pre.with(TemporalAdjusters.firstDayOfMonth());//月第一天
             LocalDateTime last=pre.with(TemporalAdjusters.lastDayOfMonth());//月最后一天
@@ -74,7 +73,7 @@ public class WfAnalyseCdqServiceImpl extends ServiceImpl<WfAnalyseCdqMapper, WfA
             postStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(last.plusDays(1));
             int i =preMonth.compareTo(nowMonth);//比较输入的月份和本月
             if (i<0){//如果查的月份比本月小
-                postStr = postStr;//查询结束就是那个月的最后一天
+                //查询结束就是那个月的最后一天
             }else {//如果查的是本月
                 postStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(nowDay.plusDays(1));//查询结束日期是今天
             }
@@ -144,67 +143,60 @@ public class WfAnalyseCdqServiceImpl extends ServiceImpl<WfAnalyseCdqMapper, WfA
         if (Objects.isNull(query)) {
             return new ArrayList<>();
         }
-        List<CdqPowerAnalysisDTO> results = new ArrayList<>();
-        String dateStrPre = query.getDateStrPre();//查询条件--开始时间
-
-        String dateStrPost = query.getDateStrPost();//查询条件--结束时间
-
-        LocalDateTime pre = DateProcessUtils.parseToLocalDateTime(dateStrPre);//开始时间转成时间格式
-
-        LocalDateTime post = DateProcessUtils.parseToLocalDateTime(dateStrPost);//结束时间转成时间格式
-
-        if (pre == null || post == null) {//判断时间是不是空的
-            return results;
+        List<CdqPowerAnalysisDTO> results=new ArrayList<>();
+        String preStr = null;
+        String postStr = null;
+        switch (query.getQueryMode()) {
+            case "day":
+                String dateStrPre = query.getDateStrPre();//查询条件--开始时间
+                String dateStrPost = query.getDateStrPost();//查询条件--结束时间
+                LocalDateTime pre = DateProcessUtils.parseToLocalDateTime(dateStrPre);//开始时间转成时间格式
+                LocalDateTime post = DateProcessUtils.parseToLocalDateTime(dateStrPost);//结束时间转成时间格式
+                if (pre == null || post == null) {//判断时间是不是空的
+                    return results;
+                }
+                preStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(pre);//开始时间转成字符串格式
+                postStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(post.plusDays(1));//结束时间填一天
+            case "month":
+                String dateStrPre1 = query.getDateStrPre();//查询条件--开始时间
+                String dateStrPreNew = dateStrPre1 + "-01";
+                LocalDateTime pre1 = DateProcessUtils.parseToLocalDateTime(dateStrPreNew);//开始时间转成时间格式
+                LocalDateTime lastDay = pre1.with(TemporalAdjusters.lastDayOfMonth());//结束时间 查询日子的月份最后一天
+                LocalDateTime first = pre1.with(TemporalAdjusters.firstDayOfMonth());//开始时间
+                if (lastDay == null) {//判断时间是不是空的
+                    return results;
+                }
+                LocalDate nowDay = LocalDate.now();//今天
+                Month preMonth = pre1.getMonth();//输入的日期的月份
+                Month nowMonth = nowDay.getMonth();//本月
+                preStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(first);//开始时间转成字符串格式
+                postStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(lastDay.plusDays(1));//结束时间填一天
+                // criteria
+                int i = preMonth.compareTo(nowMonth);//比较输入的月份和本月
+                if (i < 0) {//如果查的月份比本月小
+                    //查询结束就是那个月的最后一天
+                } else {//如果查的是本月
+                    postStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(nowDay.plusDays(1));//查询结束日期是今天
+                }
         }
-        String preStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(pre);//开始时间转成字符串格式
-
-        String postStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(post.plusDays(1));//结束时间填一天
-        String queryMode=query.getQueryMode();
-        if (queryMode.equals("day")){
-            postStr=postStr;
-        }else if (queryMode.equals("month")){
-            LocalDateTime first=pre.with(TemporalAdjusters.firstDayOfMonth());//月第一天
-            LocalDateTime last=pre.with(TemporalAdjusters.lastDayOfMonth());//月最后一天
-            if (first == null || last == null) {
-                return results;
-            }
-            LocalDate nowDay = LocalDate.now();//今天
-            Month preMonth=pre.getMonth();//输入的日期的月份
-            Month nowMonth=nowDay.getMonth();//本月
-            preStr =TimeProcessUtils.formatLocalDateTimeWithSecondPattern(first);
-            postStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(last.plusDays(1));
-            int i =preMonth.compareTo(nowMonth);//比较输入的月份和本月
-            if (i<0){//如果查的月份比本月小
-                postStr = postStr;//查询结束就是那个月的最后一天
-            }else {//如果查的是本月
-                postStr = TimeProcessUtils.formatLocalDateTimeWithSecondPattern(nowDay.plusDays(1));//查询结束日期是今天
-            }
-        }else {
-            return results;
-        }
-        // criteria
-        QueryWrapper<WfAnalyseCdq> queryWrapper = new QueryWrapper<>();//查 超短期功率分析
-
+                // criteria
+        QueryWrapper<WfAnalyseCdq> queryWrapper = new QueryWrapper<>();//查 短期功率分析
         queryWrapper.gt("calc_date", preStr);//大于开始时间
-
         queryWrapper.le("calc_date", postStr);//小于等于结束时间
-
-        queryWrapper.ge("newest", 0);//当日最新日期（0是 1否）大于等于0
-
-        List<WfAnalyseCdq> wfAnalyseCdqs = getBaseMapper().selectList(queryWrapper);
-        if (CollectionUtils.isNotEmpty(wfAnalyseCdqs)) {//判断records不为空
-            List<CdqPowerAnalysisDTO> tmp = new ArrayList<>();//创建存CdqPowerAnalysisDTO的list集合叫tmp
-            for (WfAnalyseCdq a : wfAnalyseCdqs) {//遍历wfAnalyseCdqs
-                if (Objects.isNull(a)) {//判断wfAnalyseCdqs的这个位置不为null
+        List<WfAnalyseCdq> wfAnalysedqs = getBaseMapper().selectList(queryWrapper);
+        if (CollectionUtils.isNotEmpty(wfAnalysedqs)) {//判断records不为空
+            List<CdqPowerAnalysisDTO> tmp = new ArrayList<>();//创建存dqPowerAnalysisDTO的list集合叫tmp
+            for (WfAnalyseCdq a : wfAnalysedqs) {//遍历wfAnalyseCdqs
+                if (Objects.isNull(a)) {//判断wfAnalysedqs的这个位置不为null
                     tmp.add(null);
                 }
                 if (Objects.nonNull(a)) {//如果不为null
-                    CdqPowerAnalysisDTO dtoTmp = new CdqPowerAnalysisDTO();//创建CdqPowerAnalysisDTO对象dtoTmp
+                    CdqPowerAnalysisDTO dtoTmp = new CdqPowerAnalysisDTO();//创建dqPowerAnalysisDTO对象dtoTmp
                     BeanUtils.copyProperties(a, dtoTmp);//a转成dtoTmp类
                     tmp.add(dtoTmp);//dtoTmp存进tmp
                 }
             }
-            results = tmp;
+            results=tmp;
         }
         return results;
     }
