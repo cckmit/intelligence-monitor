dbce7f3bd2344bcd2034934389f04be59b0d422d
liukai <liukai@zhikuntech.com>
2021-07-05 14:54:50 +0800

    add mapper扫描

--- unityplatform-microalarm/alarm-app/src/main/java/com/zhikuntech/intellimonitor/AlarmApplication.java
+++ unityplatform-microalarm/alarm-app/src/main/java/com/zhikuntech/intellimonitor/AlarmApplication.java
@@ -1,5 +1,6 @@
 package com.zhikuntech.intellimonitor;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author liukai
  * @date 2021/07/02
  */
+@MapperScan({"com.zhikuntech.intellimonitor.alarm.domain.mapper"})
 @SpringBootApplication
 public class AlarmApplication {
 
