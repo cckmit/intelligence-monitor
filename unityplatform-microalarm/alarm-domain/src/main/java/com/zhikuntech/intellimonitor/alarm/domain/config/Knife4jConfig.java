00b3e51cbcd094eabe5f65609ef8d7b7f9621413
liukai <liukai@zhikuntech.com>
2021-07-07 19:01:03 +0800

    add knife4j配置

--- unityplatform-microalarm/alarm-domain/src/main/java/com/zhikuntech/intellimonitor/alarm/domain/config/Knife4jConfig.java
+++ unityplatform-microalarm/alarm-domain/src/main/java/com/zhikuntech/intellimonitor/alarm/domain/config/Knife4jConfig.java
@@ -1,7 +1,5 @@
-package com.zhikuntech.intellimonitor.windpowerforecast.domain.config;
+package com.zhikuntech.intellimonitor.alarm.domain.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -18,13 +16,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 @EnableSwagger2WebMvc
 public class Knife4jConfig {
 
-//    @ConditionalOnProperty(prefix = "", name = "", havingValue = "true")
     @Bean(value = "defaultApi2")
     public Docket defaultApi2() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(new ApiInfoBuilder()
                         //.title("swagger-bootstrap-ui-demo RESTful APIs")
-                        .description("#智能监测系统接口文档-风功率预测模块")
+                        .description("#智能监测系统接口文档-告警信息模块")
 //                        .termsOfServiceUrl("http://192.168.40.14:8081")
                         .version("0.1.0.RELEASE")
                         .build())
@@ -32,7 +29,7 @@ public class Knife4jConfig {
                 .groupName("0.1版本")
                 .select()
                 //这里指定Controller扫描包路径
-                .apis(RequestHandlerSelectors.basePackage("com.zhikuntech.intellimonitor.windpowerforecast.domain.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.zhikuntech.intellimonitor.alarm.domain.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
