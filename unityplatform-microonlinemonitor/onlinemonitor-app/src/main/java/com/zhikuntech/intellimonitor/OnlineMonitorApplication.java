0f0f602b794a5b9a74217cb63842fd231d95d5a1
daizhihao <daizhihao@zhikuntech.com>
2021-07-09 10:28:59 +0800

    add mvn依赖

--- unityplatform-microonlinemonitor/onlinemonitor-app/src/main/java/com/zhikuntech/intellimonitor/OnlineMonitorApplication.java
+++ unityplatform-microonlinemonitor/onlinemonitor-app/src/main/java/com/zhikuntech/intellimonitor/OnlineMonitorApplication.java
@@ -1,8 +1,19 @@
 package com.zhikuntech.intellimonitor;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 /**
  * @author 代志豪
  * 2021/7/9 10:01
  */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class OnlineMonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMonitorApplication.class, args);
+    }
 }
