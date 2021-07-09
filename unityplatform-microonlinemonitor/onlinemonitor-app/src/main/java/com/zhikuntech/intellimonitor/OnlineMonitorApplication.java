26d601ac747f60659b81406c4b40a7e28137faa6
daizhihao <daizhihao@zhikuntech.com>
2021-07-09 10:10:19 +0800

    add package-info

---
+++ unityplatform-microonlinemonitor/onlinemonitor-app/src/main/java/com/zhikuntech/intellimonitor/OnlineMonitorApplication.java
@@ -0,0 +1,8 @@
+package com.zhikuntech.intellimonitor;
+
+/**
+ * @author 代志豪
+ * 2021/7/9 10:01
+ */
+public class OnlineMonitorApplication {
+}
---
+++ unityplatform-microonlinemonitor/onlinemonitor-prototype/src/main/java/com/zhikuntech/intellimonitor/onlinemonitor/prototype/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author 代志豪
+ * 2021/7/9 10:03
+ */
+package com.zhikuntech.intellimonitor.onlinemonitor.prototype;
