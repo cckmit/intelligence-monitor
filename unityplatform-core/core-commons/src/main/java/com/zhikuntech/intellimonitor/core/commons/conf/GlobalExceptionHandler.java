39cc8a3df70d4c8bce14f81c7f0f08a0c64a63c7
yangjincheng <jchonker@163.com>
2021-07-13 19:01:56 +0800

    add 添加常见异常处理

--- unityplatform-core/core-commons/src/main/java/com/zhikuntech/intellimonitor/core/commons/conf/GlobalExceptionHandler.java
+++ unityplatform-core/core-commons/src/main/java/com/zhikuntech/intellimonitor/core/commons/conf/GlobalExceptionHandler.java
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
+
 /**
  * @Author 杨锦程
  * @Date 2021/6/8 18:29
@@ -28,26 +30,81 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(GetSnapshotsException.class)
-    public BaseResponse handleGetSnapshotsException(GetSnapshotsException getSnapshotsException){
-        LOGGER.error(getSnapshotsException.getMessage());
-        return BaseResponse.exception(getSnapshotsException);
+    public BaseResponse handleGetSnapshotsException(GetSnapshotsException ex){
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.GOLDEN_GETSNAPSHOTS_FAILED,ex.getMessage());
     }
 
     @ExceptionHandler(SubscribeGoldenException.class)
     public BaseResponse handSubscribeGoldenException(SubscribeGoldenException ex){
-        LOGGER.error(ex.getMessage());
-        return BaseResponse.exception(ex);
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.GOLDEN_SUBSCRIBESNAPSHOTS_FAILED,ex.getMessage());
     }
 
     @ExceptionHandler(UserNotLoginException.class)
     public BaseResponse handUserNotLoginException(UserNotLoginException ex){
-        LOGGER.error(ex.getMessage());
-        return BaseResponse.exception(ex);
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.USER_NOT_LOGIN_EXCEPTION,ex.getMessage());
     }
 
     @ExceptionHandler(RemoteInterfaceCallException.class)
     public BaseResponse handRemoteInterfaceCallException(RemoteInterfaceCallException ex){
-        LOGGER.error(ex.getMessage());
-        return BaseResponse.exception(ex);
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.REMOTE_INTERFACE_CALL_EXCEPTION,ex.getMessage());
+    }
+
+
+    @ExceptionHandler(NullPointerException.class)
+    public BaseResponse handNullPointerException(NullPointerException ex){
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.NULL_POINTER_EXCEPTION,ex.getMessage());
+    }
+
+    @ExceptionHandler(ArrayIndexOutOfBoundsException.class)
+    public BaseResponse handArrayIndexOutOfBoundsException(ArrayIndexOutOfBoundsException ex){
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.ARRAY_INDEX_OUT_OF_BOUNDS_EXCEPTION,ex.getMessage());
+    }
+
+    @ExceptionHandler(ClassCastException.class)
+    public BaseResponse handClassCastException(ClassCastException ex){
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.CLASS_CAST_EXCEPTION,ex.getMessage());
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public BaseResponse handSQLException(SQLException ex){
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.SQL_EXCEPTION,ex.getMessage());
+    }
+
+    @ExceptionHandler(ArithmeticException.class)
+    public BaseResponse handArithmeticException(ArithmeticException ex){
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.ARITHMETIC_EXCEPTION,ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public BaseResponse handIllegalArgumentException(IllegalArgumentException ex){
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.ILLEGAL_ARGUMENT_EXCEPTION,ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalAccessException.class)
+    public BaseResponse handIllegalAccessException(IllegalAccessException ex){
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.ILLEGAL_ACCESS_EXCEPTION,ex.getMessage());
+    }
+
+    @ExceptionHandler(StackOverflowError.class)
+    public BaseResponse handStackOverflowError(StackOverflowError ex){
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.STACK_OVER_FLOW_ERROR,ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public BaseResponse handIllegalStateException(IllegalStateException ex){
+        LOGGER.error("异常!, msg:[{}], stack:[{}]", ex.getMessage(), ex);
+        return BaseResponse.failure(ResultCode.ILLEGAL_STATE_EXCEPTION,ex.getMessage());
     }
 }
