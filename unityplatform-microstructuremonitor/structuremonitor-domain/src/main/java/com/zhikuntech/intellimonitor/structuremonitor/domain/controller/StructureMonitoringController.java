80e024b6ce403d4d783ab7e4945e5216cdf5c259
tenan_TT <8181937+tenan-tt@user.noreply.gitee.com>
2021-07-19 14:04:31 +0800

    结构监测实时数据架构

--- unityplatform-microstructuremonitor/structuremonitor-domain/src/main/java/com/zhikuntech/intellimonitor/structuremonitor/domain/controller/StructureMonitoringController.java
+++ unityplatform-microstructuremonitor/structuremonitor-domain/src/main/java/com/zhikuntech/intellimonitor/structuremonitor/domain/controller/StructureMonitoringController.java
@@ -3,6 +3,7 @@ package com.zhikuntech.intellimonitor.structuremonitor.domain.controller;
 import com.zhikuntech.intellimonitor.core.commons.base.BaseResponse;
 import com.zhikuntech.intellimonitor.structuremonitor.domain.query.StructureMonitoringQuery;
 import com.zhikuntech.intellimonitor.structuremonitor.domain.service.IStructureMonitoringService;
+import com.zhikuntech.intellimonitor.structuremonitor.domain.vo.LiveData;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -32,8 +33,8 @@ public class StructureMonitoringController {
     @GetMapping("getDataByType")
     @ApiOperation("获取实时数据")
     @ApiImplicitParam(name = "type",value = "页签类型:1,最大,2,平均,3,最小")
-    public BaseResponse<Object> getData(@RequestParam String type) {
-       return monitoringService.getData(type);
+    public BaseResponse<List<LiveData>> getData(@RequestParam String type,@RequestParam Integer fanNumber) {
+       return monitoringService.getData(type,fanNumber);
     }
 
 
--- unityplatform-microstructuremonitor/structuremonitor-domain/src/main/java/com/zhikuntech/intellimonitor/structuremonitor/domain/vo/LiveData.java
+++ unityplatform-microstructuremonitor/structuremonitor-domain/src/main/java/com/zhikuntech/intellimonitor/structuremonitor/domain/vo/LiveData.java
@@ -1,5 +1,6 @@
 package com.zhikuntech.intellimonitor.structuremonitor.domain.vo;
 
+import com.zhikuntech.intellimonitor.core.commons.golden.annotation.GoldenId;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -20,50 +21,66 @@ import java.math.BigDecimal;
 public class LiveData {
 
     @ApiModelProperty("测点1x加速度")
+    @GoldenId(value = 1)
     private BigDecimal a1xAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点1y加速度")
     private BigDecimal a1yAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点2x加速度")
     private BigDecimal a2xAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点2y加速度")
     private BigDecimal a2yAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点3x加速度")
     private BigDecimal a3xAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点3y加速度")
     private BigDecimal a3yAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点4x加速度")
     private BigDecimal a4xAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点4y加速度")
     private BigDecimal a4yAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点5x加速度")
     private BigDecimal a5xAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点5y加速度")
     private BigDecimal a5yAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点6x加速度")
     private BigDecimal a6xAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("测点6y加速度")
     private BigDecimal a6yAcceleration;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("1沉降")
     private BigDecimal a1Subside;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("2沉降")
     private BigDecimal a2Subside;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("3沉降")
     private BigDecimal a3Subside;
 
+    @GoldenId(value = 1)
     @ApiModelProperty("4沉降")
     private BigDecimal a4Subside;
 }
